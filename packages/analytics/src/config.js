e319321217fb8e8fd74ff8abe7f36171332ae819
Alexey Zamasskin <zamasskin@yandex.ru>
2022-02-22 10:41:33 +0300

    Убрал назойливую ошибку

--- packages/analytics/src/config.js
+++ packages/analytics/src/config.js
@@ -1,6 +1,6 @@
-const apiHost = process.env.REACT_APP_API_HOST || 'http://localhost';
-const apiPort = process.env.REACT_APP_API_PORT || 8083;
-const apiPath = process.env.API_PATH || '/api';
+const apiHost = process?.env?.REACT_APP_API_HOST || 'http://localhost';
+const apiPort = process?.env?.REACT_APP_API_PORT || 8083;
+const apiPath = process?.env?.API_PATH || '/api';
 
 const config = {
     basename: '/',
