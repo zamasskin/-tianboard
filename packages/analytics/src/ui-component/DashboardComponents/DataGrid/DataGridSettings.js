c92e02cb8576ca6903f3b7e0ac3034efbf1875b5
Alexey Zamasskin <zamasskin@yandex.ru>
2022-03-01 19:36:46 +0300

    Востановил компонент DataGrid

--- packages/analytics/src/ui-component/DashboardComponents/DataGrid/DataGridSettings.js
+++ packages/analytics/src/ui-component/DashboardComponents/DataGrid/DataGridSettings.js
@@ -1,45 +1,136 @@
-import { Grid } from '@mui/material';
-import { DataGrid } from '@mui/x-data-grid';
+import _ from 'lodash';
+import { Grid, IconButton } from '@mui/material';
+import { DataGrid, GridActionsCellItem } from '@mui/x-data-grid';
+import ArrowUpwardIcon from '@mui/icons-material/ArrowUpward';
+import ArrowDownwardIcon from '@mui/icons-material/ArrowDownward';
 import Alert from '@mui/material/Alert';
 import PropTypes from 'prop-types';
 
-import { getSettingsRows, settingsColumns, getUpdateSettings, getSelection, getCodeWhenSelected, getSettings } from './helpers';
+import { getSettingsRows, getUpdateSettings, getSelection, getCodeWhenSelected, getSettings } from './helpers';
 import { useState } from 'react';
 
-const DataGridSettings = ({ data, settings, onChange }) => {
-    const [loading, setLoading] = useState(false);
-    const { error, ...parseSettings } = getSettings(data, settings);
-    if (error) {
-        return <Alert severity="error">{error.message}</Alert>;
+import { getSettingsName } from 'editor-components/dashboard/DashboardEditor/constant';
+import { defaultConfig } from './config';
+
+export const defWidth = 120;
+export const defAlign = 'left';
+export const defHeaderAlign = 'left';
+
+export const valueAlign = [
+    { value: 'left', label: 'По левому краю' },
+    { value: 'center', label: 'По центру' },
+    { value: 'right', label: 'По правому краю' }
+];
+
+// headerAlign
+export const settingsColumns = (onAction) => [
+    { field: 'id', headerName: 'Поле', width: 150, sortable: false, filterable: false },
+    { field: 'headerName', headerName: 'Название', width: 90, editable: true, sortable: false, filterable: false },
+    { field: 'width', headerName: 'Ширина', width: 80, editable: true, type: 'number', sortable: false, filterable: false },
+    {
+        field: 'align',
+        headerName: 'Выравнивание строк',
+        width: 150,
+        editable: true,
+        type: 'singleSelect',
+        valueOptions: valueAlign,
+        sortable: false,
+        filterable: false
+    },
+    {
+        field: 'headerAlign',
+        headerName: 'Выравнивание колонок',
+        width: 150,
+        editable: true,
+        type: 'singleSelect',
+        valueOptions: valueAlign,
+        sortable: false,
+        filterable: false
+    },
+    {
+        field: 'actions',
+        headerName: 'Actions',
+        type: 'actions',
+        // renderCell: RowMenuCell,
+        sortable: false,
+        width: 100,
+        headerAlign: 'center',
+        filterable: false,
+        align: 'center',
+        disableColumnMenu: true,
+        disableReorder: true,
+        getActions(props) {
+            return [
+                <IconButton aria-label="up" onClick={() => onAction('up', props)}>
+                    <ArrowUpwardIcon />
+                </IconButton>,
+                <IconButton aria-label="down" onClick={() => onAction('down', props)}>
+                    <ArrowDownwardIcon />
+                </IconButton>
+            ];
+        }
     }
+];
 
-    const onSelect = (selected) => {
+const DataGridSettings = ({ data, settings: componentSettings, onChange }) => {
+    const keys = _.chain(data).head().keys().value();
+    const settingsName = getSettingsName('DataGrid');
+    const defaultRows = keys.map((key, i) => ({ id: key, field: key, headerName: key, sort: i + 1 }));
+    const settings = _.get(componentSettings, settingsName) || {};
+    const { columnsSettings = [], selected = keys } = settings;
+    const rows = _.chain(defaultRows).keyBy('id').merge(_.keyBy(columnsSettings, 'id')).values().sortBy('sort').value();
+
+    const onEdit = (newColumnsSettings) => {
+        newColumnsSettings = _.chain(newColumnsSettings)
+            .mapValues((settingsRow) => _.mapValues(settingsRow, 'value'))
+            .value();
+        newColumnsSettings = _.merge(_.keyBy(rows, 'id'), newColumnsSettings);
+        newColumnsSettings = _.values(newColumnsSettings);
         if (onChange) {
-            setLoading(true);
-            const code = getCodeWhenSelected(selected, parseSettings);
-            onChange(code);
-            setTimeout(() => setLoading(false), 1200);
+            onChange({ ...settings, columnsSettings: newColumnsSettings });
         }
     };
 
-    const onEdit = (newSettings) => {
+    const onAction = (action, { row }) => {
+        if (action === 'up') {
+            if (row.sort > 0) {
+                row.sort -= 1.1;
+            }
+        }
+        if (action === 'down') {
+            if (row.sort < rows.length) {
+                row.sort += 1 + 0.1;
+            }
+        }
+
+        const newColumnsSettings = _.chain(rows)
+            .keyBy('id')
+            .merge({ [row.id]: row })
+            .values()
+            .sortBy('sort')
+            // .map((val) => _.omit(val, ['weight']))
+            .map((val, i) => ({ ...val, sort: i + 1 }))
+            .value();
+
         if (onChange) {
-            const code = getUpdateSettings(newSettings, parseSettings);
-            onChange(code);
+            onChange({ ...settings, columnsSettings: newColumnsSettings });
         }
     };
+
+    // console.log(rows, _.orderBy(rows, ['sort']));
+
     return (
         <Grid container>
             <Grid item xs>
                 <div style={{ height: 500, width: '100%' }}>
                     <DataGrid
                         disableSelectionOnClick
-                        rows={getSettingsRows(data, settings)}
-                        columns={settingsColumns}
+                        sort
+                        rows={_.orderBy(rows, ['sort'])}
+                        columns={settingsColumns(onAction)}
                         checkboxSelection
-                        selectionModel={getSelection(data, settings)}
-                        onSelectionModelChange={onSelect}
-                        loading={loading}
+                        selectionModel={selected}
+                        onSelectionModelChange={(selected) => onChange && onChange({ ...settings, selected })}
                         onEditRowsModelChange={onEdit}
                     />
                 </div>
--- packages/analytics/src/ui-component/DashboardComponents/DataGrid/index.js
+++ packages/analytics/src/ui-component/DashboardComponents/DataGrid/index.js
@@ -1,30 +1,35 @@
+import _ from 'lodash';
 import { Grid } from '@mui/material';
-import Alert from '@mui/material/Alert';
+import { v4 as uuidv4 } from 'uuid';
 import { DataGrid as MuiDataGrid } from '@mui/x-data-grid';
 import PropTypes from 'prop-types';
 
 import MainCard from 'ui-component/cards/MainCard';
-import { getSettings, getRows } from './helpers';
 import { gridSpacing } from 'store/constant';
+import { getSettingsName } from 'editor-components/dashboard/DashboardEditor/constant';
 
 const DataGrid = ({ data, settings }) => {
-    const componentSettings = getSettings(data, settings);
-    const { columns = [], width = '100%', height = 500, props = {}, error } = getSettings(data, settings);
-    const rows = getRows(data, componentSettings);
+    const settingsName = getSettingsName('DataGrid');
+    const keys = _.chain(data).head().keys().value();
+    const defaultColumns = _.chain(data)
+        .head()
+        .keys()
+        .map((field) => ({ field, headerName: field }))
+        .value();
+    const { columnsSettings = defaultColumns, selected = keys } = _.get(settings, settingsName) || {};
+
+    const columns = columnsSettings.filter(({ field }) => selected.includes(field));
+
+    const rows = data.map((item) => ({ ...item, id: item.id ? item.id : uuidv4() }));
 
     return (
         <MainCard>
             <Grid container spacing={gridSpacing}>
                 <Grid item xs={12}>
-                    <div style={{ width, height }}>
-                        <MuiDataGrid rows={rows} columns={columns} {...props} />
+                    <div style={{ width: '100%', height: 500 }}>
+                        <MuiDataGrid rows={rows} columns={columns} />
                     </div>
                 </Grid>
-                {error && (
-                    <Grid item xs={12}>
-                        <Alert severity="error">{error.message}</Alert>{' '}
-                    </Grid>
-                )}
             </Grid>
         </MainCard>
     );
