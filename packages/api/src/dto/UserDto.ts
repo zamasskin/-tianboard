e91c89ad379d821dcc9f9b51aba87deefe7b2adf
Alexey Zamasskin <zamasskin@yandex.ru>
2022-03-11 13:26:20 +0300

    Возвращаю пользователей

--- packages/api/src/dto/UserDto.ts
+++ packages/api/src/dto/UserDto.ts
@@ -1,15 +1,18 @@
 import { User } from "src/entities/default/User";
+import { UserRole } from "src/models/AccountModel";
 
 export class UserDto {
   id: number;
   email: string;
   firstName: string;
   secondName: string;
+  groups: UserRole[];
 
   constructor(user: User) {
     this.id = user.id;
     this.email = user.email;
     this.firstName = user.firstName;
     this.secondName = user.secondName;
+    this.groups = user.roles;
   }
 }
